b60664be46767e9bc7164841f03985239f064705
mirqwa <kimrodrikwa@gmail.com>
2024-01-27 13:48:54 +0300

    preprocess data

--- neural_net/neural_net_with_sklearn.py
+++ neural_net/neural_net_with_sklearn.py
@@ -15,18 +15,47 @@ def explore_data(df: pd.DataFrame) -> None:
     print("The number of rows and columns:", df.shape)
     print("Checking the number of columns")
     print(df.columns)
+    print("The data types")
+    print(df.dtypes)
     print("Checking the null values")
     print(df.isna().sum())
     print("Checking the unique counts")
     print(df.nunique())
 
 
+def fill_missing_values(df: pd.DataFrame) -> None:
+    df["Age"].fillna(df["Age"].mean(), inplace=True)
+    df["Fare"].fillna(df["Fare"].mean(), inplace=True)
+    df["Cabin"].fillna("", inplace=True)
+    df["Embarked"].fillna(df["Embarked"].mode()[0], inplace=True)
+
+
+def convert_categorical_to_numerical(df: pd.DataFrame) -> None:
+    df["Sex"].replace(to_replace=["male", "female"], value=[0, 1], inplace=True)
+    df["Embarked"].replace(to_replace=["C", "Q", "S"], value=[0, 1, 2], inplace=True)
+
+
+def pre_process_data(df: pd.DataFrame) -> None:
+    fill_missing_values(df)
+    convert_categorical_to_numerical(df)
+
+
 train_data = pd.read_csv("data/titanic/train.csv")
 test_data = pd.read_csv("data/titanic/test.csv")
 
+# Data exploration before preprocessing
 explore_data(train_data)
 explore_data(test_data)
+plot_correlation_matrix(train_data)
 
+# Data preprocessing
+pre_process_data(train_data)
+pre_process_data(test_data)
+
+# Data exploration after preprocessing
+print("Data exploration after pre-processing")
+explore_data(train_data)
+explore_data(test_data)
 plot_correlation_matrix(train_data)
 
 
