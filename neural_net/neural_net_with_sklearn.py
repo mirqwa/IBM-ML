5e1fd2ea84b36152d6a64ea1e958d21fab5b2175
mirqwa <kimrodrikwa@gmail.com>
2024-01-27 17:02:42 +0300

    complete testing

--- neural_net/neural_net_with_sklearn.py
+++ neural_net/neural_net_with_sklearn.py
@@ -5,6 +5,8 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
@@ -31,7 +33,7 @@ def explore_data(df: pd.DataFrame) -> None:
 
 
 def fill_missing_values(df: pd.DataFrame) -> None:
-    df["Age"].fillna(df["Age"].mean(), inplace=True)
+    df["Age"].fillna(df["Age"].median(), inplace=True)
     df["Fare"].fillna(df["Fare"].mean(), inplace=True)
     df["Cabin"].fillna(df["Cabin"].mode()[0], inplace=True)
     df["Embarked"].fillna(df["Embarked"].mode()[0], inplace=True)
@@ -53,10 +55,20 @@ def get_cabin_type(row: dict):
     return cabin_mappings.get(pattern_match.group(1)), pattern_match.group(2) or 0
 
 
+def check_if_name_has_title(name: str):
+    titles = ["master", "dr.", "rev.", "countess", "sir", "don"]
+    for title in titles:
+        if title in name.lower():
+            return 1
+    return 0
+
+
 def derive_columns(df: pd.DataFrame) -> pd.DataFrame:
     df[["CabinType", "CabinNumber"]] = df.apply(
         get_cabin_type, axis=1, result_type="expand"
     )
+    #df["NameHasTitle"] = df["Name"].apply(check_if_name_has_title)
+    df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
     return df
 
 
@@ -106,9 +118,65 @@ X_train, X_test, y_train, y_test = train_test_split(
 X_train = scale_values(X_train)
 X_test = scale_values(X_test)
 
-# trainin the model
-clf = MLPClassifier(random_state=1, max_iter=300)
-clf.fit(X_train, y_train)
+# training the model
+# hidden_layers = [(100, 100, 100), (100, 50, 10), (50, 50, 50)]
+# activations = ["identity", "logistic", "tanh", "relu"]
+# solvers = ["lbfgs", "sgd", "adam"]
+# learning_rate_inits = [0.01, 0.001, 0.0001]
+# max_score = 0
+# hyper_params = None
+# for hidden_layer in hidden_layers:
+#     for activation in activations:
+#         for solver in solvers:
+#             for learning_rate_init in learning_rate_inits:
+#                 clf = MLPClassifier(
+#                     hidden_layer_sizes=hidden_layer,
+#                     activation=activation,
+#                     solver=solver,
+#                     learning_rate_init=learning_rate_init,
+#                     random_state=1,
+#                     max_iter=100,
+#                 )
+#                 clf.fit(X_train, y_train)
 
-score = clf.score(X_test, y_test)
-print(score)
+#                 score = clf.score(X_test, y_test)
+#                 if score > max_score:
+#                     max_score = score
+#                     hyper_params = f"hidden layers: {hidden_layer}, activation: {activation}, solver: {solver}, learning_rate_init: {learning_rate_init}"
+#                 print(
+#                     f"hidden layers: {hidden_layer},",
+#                     f"activation: {activation},",
+#                     f"solver: {solver},",
+#                     f"learning_rate_init: {learning_rate_init}",
+#                     "Training score:",
+#                     clf.score(X_train, y_train),
+#                     "Test score:",
+#                     score,
+#                 )
+
+# print("The best params>>>>>>>>")
+# print(hyper_params, f"score: {max_score}")
+
+# Logistic regression model
+print("Testing the logistic model")
+penalties = ["l1", "l2", "elasticnet"]
+solvers = ["lbfgs", "liblinear", "newton-cg", "newton-cholesky", "sag"]
+for penalty in penalties:
+    for solver in solvers:
+        try:
+            clf = LogisticRegression(
+                penalty=penalty, solver=solver, random_state=0
+            ).fit(X_train, y_train)
+            print(
+                f"penalty: {penalty},",
+                f"solver: {solver},",
+                f"Train score: {clf.score(X_train, y_train)}",
+                f"test score: {clf.score(X_test, y_test)}",
+            )
+        except ValueError:
+            continue
+
+
+clf = RandomForestClassifier(random_state=0)
+clf.fit(X_train, y_train)
+print("The random forest score:", clf.score(X_test, y_test))
